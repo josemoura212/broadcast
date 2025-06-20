6d42257adb0d7795d2cc732d9ba9f890b0af380e
José Augusto <josemoura212@gmail.com>
2025-06-20 16:34:38 -0300

    ✨ feat(functions): update scheduled message processing logic

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,29 +6,34 @@ admin.initializeApp();
 export const processScheduledMessages = functions.pubsub
   .schedule("every 1 minutes")
   .onRun(async (context) => {
+    console.log("--- Início da execução da Cloud Function ---");
     const db = admin.firestore();
     const now = admin.firestore.Timestamp.now();
 
-    const clientsSnap = await db.collection("clients").get();
+    const scheduledMessagesSnap = await db
+      .collectionGroup("messages")
+      .where("status", "==", "agendada")
+      .where("scheduledAt", "<=", now)
+      .get();
 
-    for (const clientDoc of clientsSnap.docs) {
-      const messagesRef = clientDoc.ref.collection("messages");
-      const scheduledSnap = await messagesRef
-        .where("status", "==", "agendada")
-        .where("scheduledAt", "<=", now)
-        .get();
+    console.log(
+      `[CF] Encontradas ${scheduledMessagesSnap.size} mensagens agendadas para atualizar.`
+    );
 
+    if (!scheduledMessagesSnap.empty) {
       const batch = db.batch();
-      scheduledSnap.docs.forEach((msgDoc) => {
+      scheduledMessagesSnap.docs.forEach((msgDoc) => {
         batch.update(msgDoc.ref, {
           status: "enviada",
           sentAt: now,
         });
+        console.log(`[CF] Atualizando mensagem ${msgDoc.id}`);
       });
-      if (!scheduledSnap.empty) {
-        await batch.commit();
-        console.log(`Client ${clientDoc.id} - Batch committed`);
-      }
+      await batch.commit();
+      console.log("[CF] Batch comitado com sucesso.");
+    } else {
+      console.log("[CF] Nenhuma mensagem agendada para atualizar.");
     }
+    console.log("--- Fim da execução da Cloud Function ---");
     return null;
   });

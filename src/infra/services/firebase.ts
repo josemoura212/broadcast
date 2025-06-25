769afe88463876270a0524db0c9b0aaa599cbe6f
José Augusto <josemoura212@gmail.com>
2025-06-25 11:35:43 -0300

    ✨ feat(firebase): reorganizar configuração do Firebase e remover arquivo obsoleto

--- src/infra/services/firebase.ts
+++ src/infra/services/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
-import { firebaseConfig } from "../../config/firebaseConfig";
+import { firebaseConfig } from "../config/firebase-config";
 
 const app = initializeApp(firebaseConfig);
 

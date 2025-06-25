96eaa5d86c2ad9948b84f147fe70cc9d4aa4caef
José Augusto <josemoura212@gmail.com>
2025-06-24 21:51:45 -0300

    🗑️ refactor: remove unused Firebase repositories and related files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,6 @@ admin.initializeApp();
 export const processScheduledMessages = functions.pubsub
   .schedule("every 1 minutes")
   .onRun(async (context) => {
-    console.log("--- Início da execução da Cloud Function ---");
     const db = admin.firestore();
     const now = admin.firestore.Timestamp.now();
 
@@ -16,24 +15,13 @@ export const processScheduledMessages = functions.pubsub
       .where("scheduledAt", "<=", now)
       .get();
 
-    console.log(
-      `[CF] Encontradas ${scheduledMessagesSnap.size} mensagens agendadas para atualizar.`
-    );
-
     if (!scheduledMessagesSnap.empty) {
-      const batch = db.batch();
-      scheduledMessagesSnap.docs.forEach((msgDoc) => {
-        batch.update(msgDoc.ref, {
+      for (const msgDoc of scheduledMessagesSnap.docs) {
+        await msgDoc.ref.update({
           status: "enviada",
           sentAt: now,
         });
-        console.log(`[CF] Atualizando mensagem ${msgDoc.id}`);
-      });
-      await batch.commit();
-      console.log("[CF] Batch comitado com sucesso.");
-    } else {
-      console.log("[CF] Nenhuma mensagem agendada para atualizar.");
+      }
     }
-    console.log("--- Fim da execução da Cloud Function ---");
     return null;
   });

955e57c3a551a1015ec223936cf3d025e46a1da6
José Augusto <josemoura212@gmail.com>
2025-07-03 16:31:31 -0300

    ♻️ refactor(routes): reorganizar importações de componentes para melhor legibilidade

--- web/src/app/routes.tsx
+++ web/src/app/routes.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { PrivateRoute } from "./routes/private-route";
-import { Register } from "./pages/register";
-import { Home } from "./pages/home";
 import { ConnectionPage } from "./connection/page/connection-page";
 import { ContactPage } from "./contact/page/contact-page";
 import { MessagePage } from "./message/page/message-page";
 import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Home } from "./pages/home";
 
 export function AppRoutes() {
   return (
